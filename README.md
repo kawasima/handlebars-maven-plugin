80ebba039ed3bc07310bf218f70da7f4cd860111
kawasima <kawasima1016@gmail.com>
2012-06-20 12:57:12 +0900

    Added test code.

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ handlebars-maven-plugin
 =======================
 
 A maven plugin for precompiling handlebars templates
+
+Introduction
+------------
+
+handlebars-maven-plugin is used to precompile the handlebars templates of your project.
+
+Goals
+-----
+
+Goal                 |Description
+---------------------|-------------------------------
+handlebars:precompile|Precompile handlebars templates
+
+### handlebars:precompile
+
+Full name
+:net.unit8.maven.plugins:handlebars-maven-plugin:0.1.0:precompile
+
+Description
+:precomiple handlebars templates
+
+#### Optional parameters
+
+Name             |Type   |Description
+-----------------|-------|--------------------------------------
+sourceDirectory  |String |The directory of handlebars templates
+outputDirectory  |String |The directory of precompiled templates
+preserveHierarchy|Boolean|true if preserve the hierarchy of source directories.
+encoding         |String |charset of template files.
--- src/test/java/net/unit8/maven/plugins/handlebars/PrecompileMojoTest.java
+++ src/test/java/net/unit8/maven/plugins/handlebars/PrecompileMojoTest.java
@@ -1,19 +1,47 @@
 package net.unit8.maven.plugins.handlebars;
 
 import java.io.File;
+import java.io.IOException;
 
+import static org.junit.Assert.*;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrecompileMojoTest extends PrecompileMojo {
+	private PrecompileMojo mojo;
 
-	@Test
-	public void test() throws MojoExecutionException, MojoFailureException {
-		PrecompileMojo mojo = new PrecompileMojo();
+	@Before
+	public void setUp() {
+		mojo = new PrecompileMojo();
 		mojo.sourceDirectory = new File("src/test/resources/templates");
 		mojo.outputDirectory = new File("target/output");
+	}
+
+	@Test
+	public void testNotPreserveHierarchy() throws MojoExecutionException, MojoFailureException {
 		mojo.preserveHierarchy = false;
 		mojo.execute();
+		assertTrue(new File(mojo.outputDirectory, "index.js").exists());
+		assertTrue(new File(mojo.outputDirectory, "hoge.js").exists());
+		assertTrue(new File(mojo.outputDirectory, "hoge-fuga.js").exists());
+	}
+
+	@Test
+	public void testPreserveHierarchy() throws MojoExecutionException, MojoFailureException {
+		mojo.preserveHierarchy = true;
+		mojo.execute();
+		assertTrue(new File(mojo.outputDirectory, "templates.js").exists());
+		assertTrue(new File(mojo.outputDirectory, "hoge/hoge.js").exists());
+		assertTrue(new File(mojo.outputDirectory, "hoge/fuga/fuga.js").exists());
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		FileUtils.forceDelete(mojo.outputDirectory);
 	}
 }
